e19e5fcb452db54e7d69d30dc1cbe8cabb203753
nestormarlier <47480842+nestormarlier@users.noreply.github.com>
2022-08-05 23:27:13 -0300

    Listas y label en Tkinter

--- Tema 9/Ejercicio2.py
+++ Tema 9/Ejercicio2.py
@@ -1,28 +1,37 @@
-import functools
-import operator
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
 
-def impar(a):
-    if a%2!=0:
-        return a
 
-lista = []
-lista.append(int(input("Ingrese los números a evaluar 0 para terminar: ")))
+class App(tk.Tk):
+  def __init__(self):
+    super().__init__()
 
-i=0
-while lista[i] != 0:
-    valor=int(input("Ingrese los números a evaluar 0 para terminar: "))
-    if valor != 0:
-        lista.append(valor)
-        i=i+1
-        print(lista)
-    else:
-        break
+    # configure the root window
+    self.title('Mi segunda App')
+    self.geometry('300x150')
 
-print(f'Elementos ingresados: {lista}')
+    # label
+    self.label = ttk.Label(self, text='¿Estas aprendiendo Tkinter?')
+    self.label.pack()
 
-impares=list(filter(lambda x: impar(x), lista))
+   #list
+    lista = ttk.Combobox(self, width=17, state='readonly')
+    lista.place(x=30, y=80)
 
-print(f'Elementos impares {impares}')
+#desplegar
+    datos = ['Python', 'Javascript', 'Java', 'C++', 'C.Net']
+    lista['values'] = datos
+    # button
+    self.button = ttk.Button(self, text='Reset')
+    self.button['command'] = self.button_clicked
+    self.button.pack()
 
-print(f'Suma de los números impares {functools.reduce(operator.add,impares)}')
+  def button_clicked(self):
+    self.destroy()
+    self.__init__()
 
+
+if __name__ == "__main__":
+  app = App()
+  app.mainloop()
